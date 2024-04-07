15ae14c976f801834fe075a9843ebf4c06ad6965
TomaszKadziela <kadzito_aehit@students.vizja.pl>
2024-04-07 09:04:15 +0200

    zad3

--- Company.java
+++ Company.java
@@ -6,19 +6,31 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
+        // Dodawanie pracowników
+        employees[0] = new Manager("Sandra Wójcik", 50000, 0);
+        employees[1] = new Worker("Przemysław Zaremski", 60000, "Deweloper");
+        employees[2] = new Employee("Sylwia Poznańska", 55000);
+        employees[3] = new Worker("Dajana Majda", 65000, "Księgowa");
+        employees[4] = new Worker("Julita Reklińska", 70000, "Menedżer marketingu");
 
-        employees[0] = new Employee("Marek Kowalski", 3500);
-        employees[1] = new Employee("Sylwia Kopacz", 7000);
+        employees[0] = new Employee("Marek Kowalski", 3500, 0);
+        employees[1] = new Employee("Sylwia Kopacz", 7000, "Marketing");
         employees[2] = new Employee("Tomasz Kądziela", 4000);
-        employees[3] = new Employee("Kamil Kajak", 4500);
-        employees[4] = new Employee("Albert Marcel", 6300);
+        employees[3] = new Employee("Kamil Kajak", 4500, "Tester Jakości");
+        employees[4] = new Employee("Albert Marcel", 6300, "HR");
 
-        System.out.println("Dane dla pracownika o indeksie 3:");
-        System.out.println(employees[3]);
 
-        employees[3].setSalary(70000);
+        int nonManagerCount = 0;
+        for (Employee employee : employees) {
+            if (!(employee instanceof Manager)) {
+                nonManagerCount++;
+            }
+        }
+        ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
+        employees[0].setSalary(75000);
 
-        System.out.println("\nDane dla wszystkich pracowników:");
+
+        System.out.println("Dane dla wszystkich pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
         }
