e48de129ef21844675c4a68f798bbc8241a6acd1
TomaszKadziela <kadzito_aehit@students.vizja.pl>
2024-04-07 09:11:13 +0200

    .

--- Company.java
+++ Company.java
@@ -6,12 +6,11 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Employee("Marek Kowalski", 3500, 0);
-        employees[1] = new Employee("Sylwia Kopacz", 7000, "Marketing");
+        employees[0] = new Manager("Marek Kowalski", 10000, 0);
+        employees[1] = new Worker("Sylwia Kopacz", 7000, "Marketing");
         employees[2] = new Employee("Tomasz Kądziela", 4000);
-        employees[3] = new Employee("Kamil Kajak", 4500, "Tester Jakości");
-        employees[4] = new Employee("Albert Marcel", 6300, "HR");
-
+        employees[3] = new Worker("Kamil Kajak", 4500, "Tester Jakości");
+        employees[4] = new Worker("Albert Marcel", 6300, "HR");
 
         int nonManagerCount = 0;
         for (Employee employee : employees) {
