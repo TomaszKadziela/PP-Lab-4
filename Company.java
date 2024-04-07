7d43d91d48d7ced5439785b6c1eaef100ded7d95
TomaszKadziela <kadzito_aehit@students.vizja.pl>
2024-04-07 09:17:16 +0200

    .

--- Company.java
+++ Company.java
@@ -4,25 +4,25 @@ import employees.Worker;
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
         employees[0] = new Manager("Marek Kowalski", 10000, 0);
         employees[1] = new Worker("Sylwia Kopacz", 7000, "Marketing");
         employees[2] = new Employee("Tomasz Kądziela", 4000);
         employees[3] = new Worker("Kamil Kajak", 4500, "Tester Jakości");
         employees[4] = new Worker("Albert Marcel", 6300, "HR");
+        employees[5] = new Manager("Józef Kula", 13000, 0);
+        employees[6] = new Worker("Maria Jodła", 6300, "Asystent działu");
 
-        int nonManagerCount = 0;
         for (Employee employee : employees) {
-            if (!(employee instanceof Manager)) {
-                nonManagerCount++;
+            employee.setSalary(employee.getSalary() + 500);
+            if (employee instanceof Manager) {
+                ((Manager) employee).setNumberOfSubordinates(2);
+                employee.setSalary(15000);
             }
         }
-        ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
-        employees[0].setSalary(75000);
 
-
-        System.out.println("Dane dla wszystkich pracowników:");
+        System.out.println("Zaktualizowane dane pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
         }
